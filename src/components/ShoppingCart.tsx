2eedf507b0af599ca2f9f7296342f4d96e579c56
mlsniperpro <algopro.km@gmail.com>
2022-07-31 15:29:41 +0300

    The first version

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,32 @@
+import { Offcanvas, Stack} from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import {CartItem} from "../components/CartItem";
+import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+type ShopppingCartProps = {
+    isOpen: boolean;
+}
+export function ShoppingCart({ isOpen }: ShopppingCartProps){
+    const {closeCart, cartItems} = useShoppingCart()
+    return (
+        <Offcanvas show={isOpen} onHide= {closeCart} placement="end">
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Cart</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Stack gap={3}>
+                    {cartItems.map(item => (
+                        <CartItem key={item.id} {...item}/>
+                    ))}
+                    <div className="ms-auto fw-bold fs-5">
+                        Total {formatCurrency(cartItems.reduce((total, CartItem)=>{
+                            const item = storeItems.find(i=>i.id === CartItem.id.toString())
+                            return total + (item?.price || 0) * CartItem.quantity
+                        },0))}
+                    </div>
+                </Stack>
+            </Offcanvas.Body>
+        </Offcanvas>
+    )
+}
